facf048cd9d59360500b5773a1b8e515ccda29ea
kanasounna <kanasounna@gmail.com>
2019-03-28 16:27:43 -0400

    initial setup

---
+++ cogni-bank-account-managment/src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/CogniBankAccountManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.cognibank.accountmanagment.cognibankaccountmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniBankAccountManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogniBankAccountManagmentApplication.class, args);
+	}
+
+}
