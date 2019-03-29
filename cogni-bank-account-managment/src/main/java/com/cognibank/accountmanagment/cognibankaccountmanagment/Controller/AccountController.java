81f7aecf06c69b96e0f501ab11e9cdaa4d18e1c6
kanasounna <kanasounna@gmail.com>
2019-03-29 09:59:20 -0400

    initial test

---
+++ cogni-bank-account-managment/src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/Controller/AccountController.java
@@ -0,0 +1,9 @@
+package com.cognibank.accountmanagment.cognibankaccountmanagment.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AccountController {
+
+
+}
--- cogni-bank-account-managment/src/test/java/com/cognibank/accountmanagment/cognibankaccountmanagment/CogniBankAccountManagmentApplicationTests.java
+++ cogni-bank-account-managment/src/test/java/com/cognibank/accountmanagment/cognibankaccountmanagment/CogniBankAccountManagmentApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cognibank.accountmanagment.cognibankaccountmanagment;
 
+import com.cognibank.accountmanagment.cognibankaccountmanagment.Module.Account;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CogniBankAccountManagmentApplicationTests {
 
+
+
+	// To verify that user can create account for the given Id;
+
 	@Test
-	public void contextLoads() {
+
+	public void accountCreateTest() {
+		Account account = new Account();
+		account.createAccount();
+		Assert.assertNotNull(account);
+
 	}
 
 }
