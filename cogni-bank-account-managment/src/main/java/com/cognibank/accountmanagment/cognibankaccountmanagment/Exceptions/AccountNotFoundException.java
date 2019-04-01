dff100cdc49ca530366d012800d8a09ba281719f
TeplrGuy <gibbysmalls@icloud.com>
2019-04-01 07:17:47 -0400

    Add account exception handling

---
+++ cogni-bank-account-managment/src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/Exceptions/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cognibank.accountmanagment.cognibankaccountmanagment.Exceptions;
+
+public class AccountNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+    public AccountNotFoundException() { super(); }
+    public AccountNotFoundException(String message) { super(message);  }
+    public AccountNotFoundException(String message, Throwable cause) { super(message, cause); }
+    public AccountNotFoundException(Throwable cause) { super(cause); }
+}
