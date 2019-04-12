8bdd3b0dfb6cd013fc5d2d22466d8a7ebc0fad24
seshu <spcgkd@mail.umkc.edu>
2019-04-12 18:04:29 -0400

    added swagger configuration

--- src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/Controller/TransactionController.java
+++ src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/Controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.cognibank.accountmanagment.cognibankaccountmanagment.Model.Account;
 import com.cognibank.accountmanagment.cognibankaccountmanagment.Model.Transaction;
 import com.cognibank.accountmanagment.cognibankaccountmanagment.Repository.AccountRepository;
 import com.cognibank.accountmanagment.cognibankaccountmanagment.Services.TransactionService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+@Api(tags = {"Transactions API"})
+@SwaggerDefinition(
+        tags = {
+                @Tag(name = "Transaction API", description = "Transactions API ,deals with the user transactions ")
+        }
+)
 
 @RestController
 @RequestMapping("/users/accounts/")
@@ -24,6 +31,11 @@ public class TransactionController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @ApiOperation(value = "Deposits amount ", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns user Id"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     @PutMapping("deposit/{accountNumber}/{amount}")
     public String deposit(@PathVariable long accountNumber, @PathVariable double amount) {
         Account account = accountRepository.findByAccountNumber(accountNumber);
@@ -31,6 +43,11 @@ public class TransactionController {
         return account.getUserId();
     }
 
+    @ApiOperation(value = "Withdrawal amount ", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns user Id"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     @PutMapping("withdraw/{accountNumber}/{amount}")
     public String withdraw(@PathVariable long accountNumber, @PathVariable double amount) {
         Account account = accountRepository.findByAccountNumber(accountNumber);
@@ -38,28 +55,51 @@ public class TransactionController {
         transactionService.withdraw(amount, account);
         return account.getUserId();
     }
+
+    @ApiOperation(value = "Fetches account number ", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns user's account number"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     @PutMapping("getAccountNumberById/{accountId}")
-    String getAccountNumberById(@PathVariable  String accountId){
+    public String getAccountNumberById(@PathVariable  String accountId){
         return accountRepository.findById(accountId).get().getAccountNumber()+"";
     }
+
+    @ApiOperation(value = "Transaction Report ", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns Transactions"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     //@PutMapping("report/{accountNumber}/{startDate}/{endDate}")
     @PutMapping(value = {"report/{accountId}/{startDate}/{endDate}"}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public String report(@PathVariable String accountId, @PathVariable String startDate, @PathVariable String endDate) {
         List<Transaction> transactions=transactionService.report(accountId, LocalDate.parse(startDate), LocalDate.parse(endDate));
         return toStringForReport(transactions);
     }
+
+    @ApiOperation(value = "Funds Transfer ", response = Long.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns Transaction Id"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     @PutMapping("transfer/{originAccountNumber}/{destinationAccountNumber}/{amount}")
     public long  transfer(@PathVariable long originAccountNumber, @PathVariable long destinationAccountNumber, @PathVariable double amount) {
         //will return the transaction id
         return transactionService.transfer(originAccountNumber,destinationAccountNumber,amount);
     }
 
-    //xml report
+    @ApiOperation(value = "Transaction Report in XML format", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful. Returns Transactions"),
+            @ApiResponse(code = 400, message = "Badly formed request, or validations failed.")
+    })
     @PutMapping(value={"reportXML/{accountId}/{startDate}/{endDate}"},
             produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
-    public List<Transaction> reportXML(@PathVariable String accountId, @PathVariable String startDate, @PathVariable String endDate, HttpServletResponse response) {
+    public String reportXML(@PathVariable String accountId, @PathVariable String startDate, @PathVariable String endDate, HttpServletResponse response) {
         response.setHeader("Content type ","xml");
-        return transactionService.report(accountId, LocalDate.parse(startDate), LocalDate.parse(endDate));
+        List<Transaction> transactionList = transactionService.report(accountId, LocalDate.parse(startDate), LocalDate.parse(endDate));
+        return toStringForReportXML(transactionList);
     }
 
     //Json imitation
@@ -80,7 +120,8 @@ public class TransactionController {
 
 
     }
-// xml report transformer
+
+    // xml report transformer
     public String toStringForReportXML(List<Transaction> transactionList) {
         final StringBuilder result = new StringBuilder();
         transactionList.stream().forEach(tran -> {
---
+++ src/main/java/com/cognibank/accountmanagment/cognibankaccountmanagment/Model/AccountsList.java
@@ -0,0 +1,16 @@
+package com.cognibank.accountmanagment.cognibankaccountmanagment.Model;
+
+import java.util.List;
+
+public class AccountsList {
+
+    private List<Account> accounts;
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
